d41469e59b5dc9ecac344b802edffb7856327fcf
Chris Lasher <chris.lasher@gmail.com>
2011-04-04 21:34:12 +0000

    Adds parameters state for terms-based model.
    ``TermPriorParametersState`` adds an additional parameter to the space,
    ``term_prior``, which is analogous to ``link_prior``.
    
    git-svn-id: svn+ssh://cowcreamer.cs.vt.edu/home/murali/svnrepo/src/python/BiologicalProcessNetworks/trunk@3720 1fbec0df-f6b4-4bca-857c-de90862b47bd

--- bpn/mcmc/states.py
+++ bpn/mcmc/states.py
@@ -434,6 +434,158 @@ class RandomTransitionParametersState(PLNParametersState):
         return new_state
 
 
+class TermPriorParametersState(RandomTransitionParametersState):
+    """Similar to `RandomTransitionParametersState`, but includes an
+    extra parameter, the term prior probability.
+
+    Intended for use with `TermsAndLinksState`.
+
+    """
+    parameter_names = ('alpha', 'beta', 'link_prior', 'term_prior')
+    # This controls the maximum number of discrete settings we can
+    # choose from for the link prior rate
+    _size_term_prior_distribution = 20
+    _term_prior_max = 0.5
+
+    def __init__(
+            self,
+            number_of_links,
+            number_of_terms,
+            alpha=None,
+            beta=None,
+            link_prior=None,
+            term_prior=None
+        ):
+        """Create a new instance.
+
+        :Parameters:
+        - `number_of_links`: the total number of links being considered
+        - `number_of_terms`: the total number of terms being considered
+        - `alpha`: the false-positive rate, the portion of gene-gene
+          interactions which were included, but shouldn't have been
+        - `beta`: the false-negative rate, the portion of gene-gene
+          interactions which weren't included, but should have been
+        - `link_prior`: the assumed probability we would select any one
+          link
+        - `term_prior`:the assumed probability we would select any one
+          term
+
+
+        """
+        # We must first set up the link prior distribution; this cannot
+        # be known beforehand, because it depends on knowing the number
+        # links beforehand.
+        if number_of_links >= self._size_link_prior_distribution:
+            self._link_prior_distribution = [
+                    float(k) / number_of_links for k in range(1,
+                        self._size_link_prior_distribution + 1)
+            ]
+        else:
+            self._link_prior_distribution = []
+            for k in range(1, number_of_links):
+                value = float(k) / number_of_links
+                if value <= self._link_prior_max:
+                    self._link_prior_distribution.append(value)
+                else:
+                    break
+
+        if SUPERDEBUG_MODE:
+            logger.log(SUPERDEBUG, "link_prior_distribution: %s" %
+                    self._link_prior_distribution)
+
+        # We must first set up the term prior distribution; this cannot
+        # be known beforehand, because it depends on knowing the number
+        # terms beforehand.
+        if number_of_terms >= self._size_term_prior_distribution:
+            self._term_prior_distribution = [
+                    float(k) / number_of_terms for k in range(1,
+                        self._size_term_prior_distribution + 1)
+            ]
+        else:
+            self._term_prior_distribution = []
+            for k in range(1, number_of_terms):
+                value = float(k) / number_of_terms
+                if value <= self._term_prior_max:
+                    self._term_prior_distribution.append(value)
+                else:
+                    break
+
+        if SUPERDEBUG_MODE:
+            logger.log(SUPERDEBUG, "term_prior_distribution: %s" %
+                    self._term_prior_distribution)
+
+        # Set all parameters, if not set already, and validate them.
+        self._set_parameters_at_init(alpha, beta, link_prior,
+                term_prior)
+        logger.debug("Initial parameter settings: "
+                "alpha=%s, beta=%s, link_prior=%s, term_prior=%s" % (
+                    self.alpha, self.beta, self.link_prior,
+                    self.term_prior
+                )
+        )
+
+        self._parameter_selection_cutoffs = None
+
+        # This variable is used to store the previous state that the
+        # current state arrived from. When set, it should be a tuple
+        # containing the name of the parameter, and the index in the
+        # distribution that it had previous to this state.
+        self._delta = None
+
+
+    def _set_parameters_at_init(self, alpha, beta, link_prior,
+            term_prior):
+        """A helper function to verify and set the parameters at
+        instantiation.
+
+        :Parameters:
+        - `alpha`: the false-positive rate, the portion of gene-gene
+          interactions which were included, but shouldn't have been
+        - `beta`: the false-negative rate, the portion of gene-gene
+          interactions which weren't included, but should have been
+        - `link_prior`: the assumed probability we would select any one
+          link
+        - `term_prior`:the assumed probability we would select any one
+          term
+
+        """
+        given_values = locals()
+        for param_name in self.parameter_names:
+            value = given_values[param_name]
+            param_distribution = getattr(self, '_%s_distribution' %
+                    param_name)
+            param_index_name = '_%s_index' % param_name
+            if value is None:
+                # The user did not define the value ahead of time;
+                # select one randomly
+                rand_index = random.randint(0,
+                        len(param_distribution) - 1)
+                # Set the index
+                setattr(self, param_index_name, rand_index)
+                # Set the value of the parameter
+                value = param_distribution[rand_index]
+                setattr(self, param_name, value)
+            else:
+                # The user defined this parameter
+                try:
+                    # Get the index for this value and set it for the
+                    # instance
+                    index = param_distribution.index(value)
+                    setattr(self, param_index_name, index)
+                except ValueError:
+                    # The user gave a value for the parameter that is
+                    # not available in the distribution; re-raise the
+                    # exception and notify the user to supply a better
+                    # value
+                    error_message = ("The value of %s for %s is "
+                            "not available in the distribution" % (
+                                value, param_name)
+                    )
+                    raise ParameterNotInDistributionError(error_message)
+                # Set the value of the parameter
+                setattr(self, param_name, value)
+
+
 class PLNLinksState(State):
     def __init__(
             self,
