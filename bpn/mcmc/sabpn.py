e36ceb56be49815a7e9605385cc93160ccd7ad92
Phillip Whisenhunt <phillip.whisenhunt@gmail.com>
2011-04-06 02:31:00 +0000

    sabpn is the driver class used to instantiate Simulated Annealing(simulatedannealing.py) and it is the class called when the user enters sabpn in the termail. It does not record its state transitions as in mcmc, but rather outputs the best state found after annealing. After bpn has been installed, enter sabpn --steps= interactionsdata annotationsdata expressiondata, with the corresponding number of steps to anneal and the correct files.
    git-svn-id: svn+ssh://cowcreamer.cs.vt.edu/home/murali/svnrepo/src/python/BiologicalProcessNetworks/trunk@3734 1fbec0df-f6b4-4bca-857c-de90862b47bd

---
+++ bpn/mcmc/sabpn.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright (c) 2011 Chris D. Lasher & Phillip Whisenhunt
+#
+# This software is released under the MIT License. Please see
+# LICENSE.txt for details.
+
+
+"""A program to detect Process Linkage Networks using
+Simulated Annealing.
+
+"""
+
+
+import collections
+import itertools
+import sys
+
+from convutils import convutils
+
+import bpn.cli
+import bpn.structures
+from defaults import (
+        SUPERDEBUG,
+        SUPERDEBUG_MODE,
+        LINKS_FIELDNAMES,
+        PARAMETERS_FIELDNAMES,
+        TRANSITIONS_FIELDNAMES,
+        DETAILED_TRANSITIONS_FIELDNAMES
+)
+
+# Configure all the logging stuff
+import logging
+logger = logging.getLogger('bpn.sabpn')
+
+if SUPERDEBUG_MODE:
+    # A logging level below logging.DEBUG
+    logging.addLevelName(SUPERDEBUG, 'SUPERDEBUG')
+    logger.setLevel(SUPERDEBUG)
+    #stream_handler.setLevel(SUPERDEBUG)
+
+import simulatedannealing
+import states
+import recorders
+
+def main(argv=None):
+    cli_parser = bpn.cli.SaCli()
+    input_data = cli_parser.parse_args(argv)
+
+    logger.info("Constructing supporting data structures; this may "
+            "take a while...")
+    annotated_interactions = bpn.structures.AnnotatedInteractionsArray(
+            input_data.interactions_graph,
+            input_data.annotations_dict
+    )
+    logger.info("Considering %d candidate links in total." %
+            annotated_interactions.calc_num_links())
+
+    logger.info("Constructing Simulated Annealing")
+    if input_data.free_parameters:
+        logger.info("Using free parameter transitions.")
+        parameters_state_class = states.RandomTransitionParametersState
+    else:
+        parameters_state_class = states.PLNParametersState
+    if input_data.disable_swaps:
+        logger.info("Disabling swap transitions.")
+        links_state_class = states.NoSwapArrayLinksState
+    else:
+        links_state_class = states.ArrayLinksState
+    if input_data.detailed_transitions:
+        logger.info("Recording extra information for each state.")
+        transitions_csvfile = convutils.make_csv_dict_writer(
+                input_data.transitions_outfile,
+                DETAILED_TRANSITIONS_FIELDNAMES
+        )
+    else:
+        transitions_csvfile = convutils.make_csv_dict_writer(
+                input_data.transitions_outfile,
+                TRANSITIONS_FIELDNAMES
+        )
+    sa = simulatedannealing.ArraySimulatedAnnealing(
+            annotated_interactions,
+            input_data.activity_threshold,
+            input_data.transition_ratio,
+            num_steps=input_data.steps,
+            parameters_state_class=parameters_state_class,
+            links_state_class=links_state_class
+    )
+    logger.info("Beginning to Anneal. This may take a while...")
+    sa.run()
+    logger.info("Run completed.")
+
+    logger.info("Writing link results to %s" %
+            input_data.links_outfile.name)
+    links_out_csvwriter = convutils.make_csv_dict_writer(
+            input_data.links_outfile, LINKS_FIELDNAMES)
+    logger.info("Writing parameter results to %s" % (
+            input_data.parameters_outfile.name))
+    parameters_out_csvwriter = convutils.make_csv_dict_writer(
+            input_data.parameters_outfile, PARAMETERS_FIELDNAMES)
+    logger.info("Writing transitions data to %s." % (
+            input_data.transitions_outfile.name))
+    logger.info("Finished.")
+
+
+if __name__ == '__main__':
+    main()
